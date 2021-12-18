8eab66cc216a10da3f92962a264b50233a7a702b
Will W <william86370@gmail.com>
2021-12-18 13:52:00 -0500

    Motified CRD name of cert-manager

--- rke2-helm/cert-manager/cert-manager.sh
+++ rke2-helm/cert-manager/cert-manager.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 # Downloads the helmchart CRD from URL
 # Adds it to RKE2 Deployments folder /var/lib/rancher/rke2/server/manifests
-CHART_VERSION=cert-manager/v1.6.1
-CHART_NAME=cert-manager-crd.yaml
 
-MANIFEST_URL=https://raw.githubusercontent.com/william86370/rke2-tools/main/rke2-helm/${CHART_VERSION}/${CHART_NAME}
-curl -sfL $MANIFEST_URL >> /var/lib/rancher/rke2/server/manifests/${CHART_NAME}
-echo "${CHART_VERSION} Installed"
+install_chart () {
+MANIFEST_URL=${BASE_URL}/${CHART_REPO}/${CHART_VERSION}/${CHART_NAME}
+rm /var/lib/rancher/rke2/server/manifests/${CHART_NAME}.yaml
+curl -sfL $MANIFEST_URL >> /var/lib/rancher/rke2/server/manifests/${CHART_NAME}.yaml
+echo "${CHART_NAME} ${CHART_VERSION} Installed"
+}
+
+BASE_URL=https://raw.githubusercontent.com/william86370/rke2-tools/main/rke2-helm
+
+#Install Cert-manager-CRDS v1.6.1
+CHART_REPO=cert-manager
+CHART_VERSION=v1.6.1
+CHART_NAME=rke2-cert-manager-crds
+install_chart 
+
+#Install Cert-manager v1.6.1
+CHART_REPO=cert-manager
+CHART_VERSION=v1.6.1
+CHART_NAME=rke2-cert-manager
+install_chart
+
+# curl -sfL https://raw.githubusercontent.com/william86370/rke2-tools/main/rke2-helm/cert-manager/cert-manager.sh | sh -
+# Delete The new addon
+# kubectl delete -n kube-system addon rke2-cert-manager-crds
